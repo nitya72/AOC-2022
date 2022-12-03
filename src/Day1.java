6512871beb1a75878b6afda02278eedc4df6dacd
Nitya Arora <nitya.arora@thoughtworks.com>
2022-12-04 00:25:58 +0530

    Initial commit

---
+++ src/Day1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+
+class Day1
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        List<Long> list = new ArrayList<>();
+        long sum = 0;
+        for(int i=0;i<2235;i++) {
+            String st = sc.nextLine();
+            if(st.length() > 0) {
+                int value = Integer.parseInt(st);
+                sum += value;
+            } else {
+                list.add(sum);
+                sum = 0;
+            }
+        }
+        list.add(sum);
+        Collections.sort(list);
+        int size = list.size();
+        long res = list.get(size-1) + list.get(size-2) + list.get(size-3);
+        System.out.println(res);
+    }
+}
---
+++ src/Day3.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class Day3 {
+    // vJrwpWtwJgWrhcsFMMfFFhFp -> 24 -> 12
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for(int i=0;i<100;i++) {
+            String s1= sc.nextLine();
+            String s2= sc.nextLine();
+            String s3= sc.nextLine();
+
+            int[] hash1 = new int[53];
+            int[] hash2 = new int[53];
+
+            for(int j=0;j<s1.length();j++) {
+                char ch = s1.charAt(j);
+                if(ch >= 'a' && ch <= 'z') {
+                    hash1[ch-'a']++;
+                } else {
+                    hash1[ch-'A'+26]++;
+                }
+            }
+
+            for(int j=0;j<s2.length();j++) {
+                char ch = s2.charAt(j);
+                if(ch >= 'a' && ch <= 'z') {
+                    hash2[ch-'a']++;
+                } else {
+                    hash2[ch-'A'+26]++;
+                }
+            }
+
+            for(int j=0;j<s3.length();j++) {
+                char ch = s3.charAt(j);
+                if(ch >= 'a' && ch <= 'z') {
+                    if(hash1[ch-'a'] > 0 && hash2[ch-'a'] > 0) {
+                        sum += ch-'a'+1;
+                        break;
+                    }
+                } else {
+                    if(hash1[ch-'A'+26] > 0 && hash2[ch-'A'+26] > 0) {
+                        sum += ch-'A'+27;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    void part1() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for(int i=0;i<300;i++) {
+            String st = sc.nextLine();
+            int length = st.length();
+            String part1 = st.substring(0, length/2);
+            String part2 = st.substring(length/2);
+            Set<Character> set = new HashSet<>();
+            int[] hash = new int[53];
+
+            for(int j=0;j<part1.length();j++) {
+                char ch = part1.charAt(j);
+                if(ch >= 'a' && ch <= 'z') {
+                    hash[ch-'a']++;
+                } else {
+                    hash[ch-'A'+26]++;
+                }
+            }
+
+            for(int j=0;j<part2.length();j++) {
+                char ch = part2.charAt(j);
+                if(ch >= 'a' && ch <= 'z') {
+                    if(hash[ch-'a'] > 0 && !set.contains(ch)) {
+                        set.add(ch);
+                        sum += ch-'a'+1;
+                    }
+                } else {
+                    if(hash[ch-'A'+26] > 0 && !set.contains(ch)) {
+                        set.add(ch);
+                        sum += ch-'A'+27;
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
