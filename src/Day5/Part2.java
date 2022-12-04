5a209a613de7895ae4cea4bfb5c63004d39eba3a
Nitya Arora <nitya.arora@thoughtworks.com>
2022-12-04 12:56:55 +0530

    Add day5 template

---
+++ src/Day5/Part2.java
@@ -0,0 +1,14 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class Part2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+}
